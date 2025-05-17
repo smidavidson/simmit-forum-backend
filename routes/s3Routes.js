a145eee5bfaa5b4357c51c76453895f5eb30d5d1
smidavidson <87210759+smidavidson@users.noreply.github.com>
2025-05-16 23:11:02 -0700

    Add crypto import

--- routes/s3Routes.js
+++ routes/s3Routes.js
@@ -2,6 +2,7 @@ import {Router} from 'express';
 import { isAuthenticated } from '../middleware/auth.js';
 import {DeleteObjectCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import crypto from 'crypto';
 
 export const s3Routes = Router();
 
